0e5f31078abe03b301fc98259af643e8ed041b2f
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-01-20 15:40:59 -0300

    Made changes to most the M4_A python directory, giving files cleaner names and i finished some stuff as well.

---
+++ Python/M4_A/csv_salary_report.py
@@ -0,0 +1,22 @@
+import csv
+
+with open("data.csv","r") as infile, open("high_earners.csv", "w", newline="") as outfile, open ("low_earners.csv", "w") as outfile_2: 
+    reader = csv.reader(infile)
+    writer = csv.writer(outfile)
+    writer_2 = csv.writer(outfile_2)
+    header = next(reader)
+    writer.writerow(header)
+    writer_2.writerow(header)
+
+    for row in reader:
+        name = row[0]
+
+        try:
+            salary = int(row[2])
+        except:
+            continue
+
+        if salary >= 4000:
+            writer.writerow(row)
+        else:
+            writer_2.writerow(row)
