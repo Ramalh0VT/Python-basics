bffdc4858d2cf9948d96bc35c6c79fe05736b620
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-01-31 09:11:18 -0300

    Made changes to the task manager project that i didn't commit and push

--- modular_programming/task_manager/task_app/logic/actions.py
+++ modular_programming/task_manager/task_app/logic/actions.py
@@ -1,7 +1,21 @@
+from 
+
 def create_task():
-    task = input("Type your task: ")
-    if task == "":
-        print("You must type something, the task can't be nothing")
+    while True:
+        task = input("Type your task: \n")
+        if task == "":
+            print("You must type something, the task can't be nothing")
+        else:
+            break
+        
+    
+def remove_task():
+    while True:
+        task_to_remove = input("Type the task you want to be removed: \n")
+        if task_to_remove = "":
+            print("You must type something, you can't remove an empty task.")
+        else:
+            break
 
 if __name__ == "__main__":
     create_task()
